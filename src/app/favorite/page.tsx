8ced808f94b8364ed89862cf607ba599562d5438
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2024-12-16 23:09:15 +0900

    feet: 카드 구현

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -1,3 +1,10 @@
 export default function Favorite() {
-  return <h1>favorite</h1>;
+  return (
+    <div>
+      <div className="w-full bg-[#F0F6FF] pb-[60px]">
+        <h1 className="text-center text-[40px] font-semibold">⭐️ 즐겨찾기</h1>
+      </div>
+      <div>{/* 카드 */}</div>
+    </div>
+  );
 }
