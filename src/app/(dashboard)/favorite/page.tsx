8ce23ed868d0ac1a254fbc799f49df371233df3f
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2024-12-25 22:48:42 +0900

    .

--- src/app/(dashboard)/favorite/page.tsx
+++ src/app/(dashboard)/favorite/page.tsx
@@ -1,38 +1,38 @@
 'use client';
 
-import { getFavorites } from '@/api/api';
+// import { getFavorites } from '@/api/api';
 import Card from '@/components/Card';
-import Pagination from '@/components/Pagenation';
-import { CardListResponse, LinkResponse } from '@/types/api';
+// import Pagination from '@/components/Pagenation';
+// import { CardListResponse, LinkResponse } from '@/types/api';
 import { useAuth } from '@/utils/AuthContext';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 export default function Favorite() {
   const { accessToken } = useAuth();
-  const [activePage, setActivePage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const itemsPerPage = 9; // 한 페이지에 표시할 카드 개수
+  // const [activePage, setActivePage] = useState(1);
+  // const [totalPages, setTotalPages] = useState(1);
+  // const itemsPerPage = 9; // 한 페이지에 표시할 카드 개수
 
   // const [favorites, setFavorites] = useState<LinkResponse[]>([]);
 
-  useEffect(() => {
-    // 총 즐겨찾기 개수를 가져오는 API 호출
-    const fetchTotalPages = async () => {
-      if (!accessToken) return;
+  // useEffect(() => {
+  //   // 총 즐겨찾기 개수를 가져오는 API 호출
+  //   const fetchTotalPages = async () => {
+  //     if (!accessToken) return;
 
-      try {
-        const data: CardListResponse = await getFavorites('11-4', 1, itemsPerPage); // 첫 페이지 데이터 요청
-        if (data && data.totalCount) {
-          const totalItems = data.totalCount;
-          setTotalPages(Math.ceil(totalItems / itemsPerPage)); // 전체 페이지 수 계산
-        }
-      } catch (error) {
-        console.error('총 즐겨찾기 수를 가져오는데 실패했습니다.', error);
-      }
-    };
+  //     try {
+  //       const data: CardListResponse = await getFavorites('11-4', 1, itemsPerPage); // 첫 페이지 데이터 요청
+  //       if (data && data.totalCount) {
+  //         const totalItems = data.totalCount;
+  //         setTotalPages(Math.ceil(totalItems / itemsPerPage)); // 전체 페이지 수 계산
+  //       }
+  //     } catch (error) {
+  //       console.error('총 즐겨찾기 수를 가져오는데 실패했습니다.', error);
+  //     }
+  //   };
 
-    fetchTotalPages();
-  }, [accessToken]);
+  //   fetchTotalPages();
+  // }, [accessToken]);
 
   // useEffect(() => {
   //   const fetchPageData = async () => {
@@ -51,9 +51,9 @@ export default function Favorite() {
   //   fetchPageData();
   // }, [accessToken, activePage]);
 
-  const onPageChange = (pageNumber: number) => {
-    setActivePage(pageNumber);
-  };
+  // const onPageChange = (pageNumber: number) => {
+  //   setActivePage(pageNumber);
+  // };
 
   return (
     <div>
@@ -65,11 +65,11 @@ export default function Favorite() {
           <div>
             <Card folderId={null} />
             <div className='mt-[32px] mb-[60px] md:mt-[40px] md:mb-[97px] lg:mb-[]'>
-              <Pagination
+              {/* <Pagination
                 totalPageNum={totalPages}
                 activePageNum={activePage}
                 onPageChange={onPageChange}
-              />
+              /> */}
             </div>
           </div>
         ) : (
