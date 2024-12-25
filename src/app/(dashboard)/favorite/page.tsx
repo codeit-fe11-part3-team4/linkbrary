8577237eb917671707ad5bef33e70cc80debfbb0
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2024-12-25 22:44:42 +0900

    .

--- src/app/(dashboard)/favorite/page.tsx
+++ src/app/(dashboard)/favorite/page.tsx
@@ -13,7 +13,7 @@ export default function Favorite() {
   const [totalPages, setTotalPages] = useState(1);
   const itemsPerPage = 9; // 한 페이지에 표시할 카드 개수
 
-  const [favorites, setFavorites] = useState<LinkResponse[]>([]);
+  // const [favorites, setFavorites] = useState<LinkResponse[]>([]);
 
   useEffect(() => {
     // 총 즐겨찾기 개수를 가져오는 API 호출
@@ -34,22 +34,22 @@ export default function Favorite() {
     fetchTotalPages();
   }, [accessToken]);
 
-  useEffect(() => {
-    const fetchPageData = async () => {
-      if (!accessToken) return;
+  // useEffect(() => {
+  //   const fetchPageData = async () => {
+  //     if (!accessToken) return;
 
-      try {
-        const data: CardListResponse = await getFavorites('11-4', activePage, itemsPerPage); // 해당 페이지 데이터 요청
-        if (data && data.list) {
-          setFavorites(data.list); // 리스트 데이터를 상태에 저장
-        }
-      } catch (error) {
-        console.error('페이지 데이터를 가져오는데 실패했습니다.', error);
-      }
-    };
+  //     try {
+  //       const data: CardListResponse = await getFavorites('11-4', activePage, itemsPerPage); // 해당 페이지 데이터 요청
+  //       if (data && data.list) {
+  //         setFavorites(data.list); // 리스트 데이터를 상태에 저장
+  //       }
+  //     } catch (error) {
+  //       console.error('페이지 데이터를 가져오는데 실패했습니다.', error);
+  //     }
+  //   };
 
-    fetchPageData();
-  }, [accessToken, activePage]);
+  //   fetchPageData();
+  // }, [accessToken, activePage]);
 
   const onPageChange = (pageNumber: number) => {
     setActivePage(pageNumber);
