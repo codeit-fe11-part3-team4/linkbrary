e5345f8d97de06d267f210e19170ce09de3b1ff3
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2024-12-25 22:33:03 +0900

    페이지네이션 수정 중(미완료)

--- src/app/(dashboard)/favorite/page.tsx
+++ src/app/(dashboard)/favorite/page.tsx
@@ -3,6 +3,7 @@
 import { getFavorites } from '@/api/api';
 import Card from '@/components/Card';
 import Pagination from '@/components/Pagenation';
+import { CardListResponse, LinkResponse } from '@/types/api';
 import { useAuth } from '@/utils/AuthContext';
 import { useEffect, useState } from 'react';
 
@@ -12,13 +13,15 @@ export default function Favorite() {
   const [totalPages, setTotalPages] = useState(1);
   const itemsPerPage = 9; // 한 페이지에 표시할 카드 개수
 
+  const [favorites, setFavorites] = useState<LinkResponse[]>([]);
+
   useEffect(() => {
     // 총 즐겨찾기 개수를 가져오는 API 호출
     const fetchTotalPages = async () => {
       if (!accessToken) return;
 
       try {
-        const data = await getFavorites('11-4', 1, itemsPerPage); // 첫 페이지 데이터 요청
+        const data: CardListResponse = await getFavorites('11-4', 1, itemsPerPage); // 첫 페이지 데이터 요청
         if (data && data.totalCount) {
           const totalItems = data.totalCount;
           setTotalPages(Math.ceil(totalItems / itemsPerPage)); // 전체 페이지 수 계산
@@ -30,6 +33,23 @@ export default function Favorite() {
 
     fetchTotalPages();
   }, [accessToken]);
+
+  useEffect(() => {
+    const fetchPageData = async () => {
+      if (!accessToken) return;
+
+      try {
+        const data: CardListResponse = await getFavorites('11-4', activePage, itemsPerPage); // 해당 페이지 데이터 요청
+        if (data && data.list) {
+          setFavorites(data.list); // 리스트 데이터를 상태에 저장
+        }
+      } catch (error) {
+        console.error('페이지 데이터를 가져오는데 실패했습니다.', error);
+      }
+    };
+
+    fetchPageData();
+  }, [accessToken, activePage]);
 
   const onPageChange = (pageNumber: number) => {
     setActivePage(pageNumber);
