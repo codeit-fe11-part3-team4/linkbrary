1aa008d9012d9487d6ed185211b4f1368e76fcaa
석지우 <jiwoo9665@gmail.com>
2024-12-25 23:16:40 +0900

    style: 인풋 스타일 추가

--- src/components/LinkPage/SearchInput.tsx
+++ src/components/LinkPage/SearchInput.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import { useState } from 'react';
+import IconSearch from '../../../public/icons/ic_search.svg';
+import IconSearchGray from '../../../public/icons/ic_Search_gray.svg';
+import Image from 'next/image';
 
 type SearchInputProps = {
   onSearch: (query: string) => void;
@@ -12,32 +15,49 @@ export default function SearchInput({ onSearch }: SearchInputProps) {
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const query = event.target.value;
     setSearchQuery(query);
-    onSearch(query); // 검색어 상태 변경 시 부모 컴포넌트로 전달
+    onSearch(query);
   };
 
   const handleClear = () => {
-    setSearchQuery(''); // 검색어를 빈 값으로 초기화
-    onSearch(''); // 부모 컴포넌트로도 빈 값 전달
+    setSearchQuery('');
+    onSearch('');
   };
 
   return (
-    <div className="relative">
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={handleInputChange}
-        placeholder="검색어를 입력하세요..."
-        className="w-full px-4 py-2 border rounded"
-      />
+    <div className='mt-[40px]'>
+      <div className="flex items-center bg-[#F5F5F5] rounded-[10px] p-2 relative mb-[40px] pt-[15px] pb-[15px] pr-[16px] pl-[16px]">
+        {/* 검색 아이콘 */}
+        <Image
+          src={searchQuery ? IconSearch : IconSearchGray}
+          alt="검색아이콘"
+          className="w-6 h-6 mr-2"
+        />
+
+        {/* 검색 입력 필드 */}
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleInputChange}
+          placeholder="링크를 검색해 보세요."
+          className="flex-grow bg-transparent outline-none placeholder:text-[#666666] text-black "
+        />
+
+        {/* X 버튼 */}
+        {searchQuery && (
+          <button
+            onClick={handleClear}
+            className="w-6 h-6 flex items-center justify-center rounded-full bg-[#CCD5E3] text-white text-sm hover:opacity-80"
+          >
+            ✕
+          </button>
+        )}
+      </div>
       {searchQuery && (
-        <button
-          onClick={handleClear}
-          className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-black"
-        >
-          ✕
-        </button>
+        <p className="text-[32px] font-semibold mb-[40px]">
+          <span className="text-black">{searchQuery}</span>
+          <span className="text-[#9FA6B2]">으로 검색한 결과입니다.</span>
+        </p>
       )}
-      {searchQuery && <p>{searchQuery}으로 검색한 결과입니다.</p>}
     </div>
   );
 }
