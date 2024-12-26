8b3d27bdedae9f5f3abec65338c37cf029a087de
석지우 <jiwoo9665@gmail.com>
2024-12-26 13:59:08 +0900

    style: 복사 알림 스타일 수정

--- src/components/LinkPage/FolderEdit.tsx
+++ src/components/LinkPage/FolderEdit.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import Kakao from "../../../public/icons/kakao.svg";
 import facebook from "../../../public/icons/ic_facebook.svg";
-import link from "../../../public/icons/ic_link.svg";
+import link from "../../../public/icons/link.svg";
 import Iconshare from "../../../public/icons/share.svg";
 //import Iconpen from "../../../public/icons/pen.svg";
 //import Icondelete from "../../../public/icons/delete.svg";
@@ -85,21 +85,24 @@ export default function FolderEdit({ folderName }: { folderName: string }) {
   
         {/* 복사 성공 알림 */}
         {copied && (
-          <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 flex items-center gap-2 rounded bg-white px-4 py-2 text-black shadow-md">
+        <div
+            className={`fixed top-4 left-1/2 transform -translate-x-1/2 z-50 flex items-center gap-2 rounded bg-white px-4 py-2 text-black shadow-md 
+            transition-opacity duration-500 ease-in-out ${copied ? 'opacity-100' : 'opacity-0'}`}
+        >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-green"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-green-500" // 아이콘 색상을 초록색으로 설정
+            viewBox="0 0 20 20"
+            fill="currentColor"
             >
-              <path
+            <path
                 fillRule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-10.707a1 1 0 00-1.414-1.414L9 10.172 7.707 8.879a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                 clipRule="evenodd"
-              />
+            />
             </svg>
             <span>링크가 복사되었습니다</span>
-          </div>
+        </div>
         )}
       </div>
     );
