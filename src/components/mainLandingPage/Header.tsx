fd47c89cf1fcb58d406d276bf70265811e7b32d8
석지우 <jiwoo9665@gmail.com>
2024-12-29 00:57:45 +0900

    style: 헤더

--- src/components/mainLandingPage/Header.tsx
+++ src/components/mainLandingPage/Header.tsx
@@ -1,15 +1,19 @@
-import Logo from "./Logo";
+import logo from "../../../public/icons/logo.svg";
 import LoginButton from "./LoginButton";
 import styles from "./Header.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <Logo />
-      </div>
-      <div className={styles["login-button"]}>
-        <LoginButton />
+      <div className={styles.headerContainer}>
+        <Link href="/">
+          <Image src={logo} alt="logo" className={styles.logoImage}/>
+        </Link>
+        <div className={styles["login-button"]}>
+          <LoginButton />
+        </div>
       </div>
     </header>
   );
