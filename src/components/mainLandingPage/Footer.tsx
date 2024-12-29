b20fe766dcd93820206353564f8fd0eb657894f4
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2024-12-29 16:42:45 +0900

    footer css 수정

--- src/components/mainLandingPage/Footer.tsx
+++ src/components/mainLandingPage/Footer.tsx
@@ -1,20 +1,21 @@
-import Link from "next/link";
 import Image from "next/image";
-import styles from "./Footer.module.css";
+import Link from "next/link";
 
-const Footer = () => {
+function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.copyright}>©codeit - 2024</div>
-      <div className={styles["footer-menu"]}>
-        <Link href="/privacy" className={styles.link}>
+    <footer className="bg-[#111322] flex flex-wrap justify-between items-center w-full mt-[120px] p-[32px] md:px-[104px] md:pb-[110px] lg:pb-[108px]">
+      <div className="text-[#676767] whitespace-nowrap order-3 w-full mt-[60px] md:mt-0 md:order-1 md:w-auto">
+        ©codeit - 2024
+      </div>
+      <div className="flex items-center gap-[30px] whitespace-nowrap order-1">
+        <Link href="/privacy" className="text-[#cfcfcf] no-underline">
           Privacy Policy
         </Link>
-        <Link href="/faq" className={styles.link}>
+        <Link href="/faq" className="text-[#cfcfcf] no-underline">
           FAQ
         </Link>
       </div>
-      <div className={styles["social-media"]}>
+      <div className="flex gap-[12px] lg:gap-[13px] order-2 md:order-3">
         <a
           href="https://www.facebook.com/"
           target="_blank"
@@ -23,10 +24,9 @@ const Footer = () => {
         >
           <Image
             src="/icons/facebook.svg"
-            alt="페이스북"
             width={20}
             height={20}
-            className={styles["social-icon"]}
+            alt="페이스북"
           />
         </a>
         <a
@@ -37,10 +37,9 @@ const Footer = () => {
         >
           <Image
             src="/icons/twitter.svg"
-            alt="트위터"
             width={20}
             height={20}
-            className={styles["social-icon"]}
+            alt="트위터"
           />
         </a>
         <a
@@ -51,10 +50,9 @@ const Footer = () => {
         >
           <Image
             src="/icons/youtube.svg"
-            alt="유튜브"
             width={20}
             height={20}
-            className={styles["social-icon"]}
+            alt="유튜브"
           />
         </a>
         <a
@@ -65,15 +63,14 @@ const Footer = () => {
         >
           <Image
             src="/icons/instagram.svg"
-            alt="인스타그램"
             width={20}
             height={20}
-            className={styles["social-icon"]}
+            alt="인스타그램"
           />
         </a>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
